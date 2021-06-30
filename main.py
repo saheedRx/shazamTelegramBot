442690139ce007412568ca54307231fea5469b11
Illia Stasiuk <illia.stasiuk@protonmail.com>
2021-06-30 23:26:42 +0300

    Ready-to-use bot, replies with song name and cover to voice or audio

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-import telebot
-import asyncio
-from shazamio import Shazam
+import os
+from pprint import pprint
+import shutil
 
-TOKEN = None
+import telebot
+import requests
+import asyncio
+from shazamio import Shazam, serialize_track
+
+TEMP_FOLDER = ".temp"
 
 with open("TOKEN") as f:
     TOKEN = f.read().strip()
 
 bot = telebot.TeleBot(TOKEN, parse_mode=None)
 
+
 @bot.message_handler(commands=["start"])
 def welcome(message):
-    bot.reply_to(message, "Hello")
+    bot.send_message(message.chat.id, "Send audio or voice message to start using the bot")
 
+
+def download_file_and_return_path(cache_id, file_id):
+    file_info = bot.get_file(file_id)
+    filename = file_info.file_path.split("/")[-1]
+    resp = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_info.file_path))
+    folder = os.path.join(TEMP_FOLDER, str(cache_id))
+    os.mkdir(folder)
+    filepath = os.path.join(folder, filename)
+    with open(filepath, "wb") as f:
+        f.write(resp.content)
+    return filepath
+
+
+def download_cover_and_return_path(cache_id, url):
+    name = url.split("/")[-1]
+    resp = requests.get(url)
+    folder = os.path.join(TEMP_FOLDER, str(cache_id))
+    filepath = os.path.join(folder, name)
+    with open(filepath, "wb") as f:
+        f.write(resp.content)
+    return filepath
+
+
+async def recognize(path):
+    shazam = Shazam()
+    out = await shazam.recognize_song(path)
+    return out
+
+
+@bot.message_handler(content_types=["audio", "voice"])
+def handle_audio(message):
+    type_is_voice = message.content_type == "voice"
+    file_id = message.voice.file_id if type_is_voice else message.audio.file_id
+    duration = message.voice.duration if type_is_voice else message.audio.duration
+    file_local_path = download_file_and_return_path(message.id, file_id)
+    data = loop.run_until_complete(recognize(file_local_path))
+    # pprint(data)
+
+    if duration > 15:
+        bot.reply_to(message, "Please try sending a shorter sample")
+    elif not data["matches"]:
+        bot.reply_to(message, "Sorry, couldn't recognize any song. Try sending a longer sample")
+    else:
+        track = serialize_track(data['track'])
+        caption = f"{track.subtitle} - {track.title}"
+        # photo_url = track.photo_url
+        photo_url = data['track']['images']['coverarthq']
+        if photo_url:
+            cover_path = download_cover_and_return_path(message.id, photo_url)
+            cover = open(cover_path, "rb")
+            bot.send_photo(message.chat.id, cover, caption=caption, reply_to_message_id=message.id)
+            cover.close()
+        else:
+            bot.reply_to(message, caption)
+    shutil.rmtree(os.path.join(TEMP_FOLDER, str(message.id)))
+
+
+loop = asyncio.get_event_loop()
 bot.polling()
